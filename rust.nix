82a04b73d32db9c39ddbba5fd13a204b1870a6ac
Iain Calder <iain.calder@gmail.com>
2025-09-06 21:34:24 +0000

    fix: export RUST_SRC_PATH for tools that need it

--- rust.nix
+++ rust.nix
@@ -9,4 +9,10 @@ nixpkgs.legacyPackages.x86_64-linux.mkShell {
     rustfmt
     clippy
   ];
+
+  # Certain Rust tools won't work without this
+  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+  RUST_SRC_PATH = "${nixpkgs.legacyPackages.x86_64-linux.rust.packages.stable.rustPlatform.rustLibSrc
+  }";
 }
