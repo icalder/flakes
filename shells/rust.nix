f14c2846ed6bfb01713c4d08b93a2d7815843680
Iain Calder <iain.calder@gmail.com>
2026-02-02 21:45:07 +0000

    feat: added openssl and pkg-config to rust shell

--- shells/rust.nix
+++ shells/rust.nix
@@ -8,11 +8,12 @@ nixpkgs.legacyPackages.${system}.mkShell {
     rust-analyzer
     rustfmt
     clippy
+    openssl
+    pkg-config
   ];
 
   # Certain Rust tools won't work without this
   # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
   # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-  RUST_SRC_PATH = "${nixpkgs.legacyPackages.${system}.rust.packages.stable.rustPlatform.rustLibSrc
-  }";
+  RUST_SRC_PATH = "${nixpkgs.legacyPackages.${system}.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
